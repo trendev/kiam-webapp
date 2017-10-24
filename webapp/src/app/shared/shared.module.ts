7350f5aac5e06395404608fe7a0a1abee371cbad
jsie <julien.sie@gmail.com>
2017-10-24 14:57:16 +0200

    adds imports in Core&Shared

--- webapp/src/app/shared/shared.module.ts
+++ webapp/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [PageNotFoundComponent],
   exports: [PageNotFoundComponent]
