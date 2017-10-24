7da297ad2b4d6e450a5fcbded29f6b8805084be6
jsie <julien.sie@gmail.com>
2017-10-24 15:15:16 +0200

    inits LoginComponent

--- webapp/src/app/login/login.module.ts
+++ webapp/src/app/login/login.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule,
     LoginRoutingModule
   ],
-  declarations: []
+  declarations: [LoginComponent],
+  exports: [LoginComponent]
 })
 export class LoginModule { }
