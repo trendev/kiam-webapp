afcfabdb378598704e9397c1fae1faba17123125
jsie <julien.sie@gmail.com>
2017-10-24 15:11:53 +0200

    inits Administrator/Pro/Ind/Login modules

---
+++ webapp/src/app/individual/individual.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IndividualRoutingModule } from './individual-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    IndividualRoutingModule
+  ],
+  declarations: []
+})
+export class IndividualModule { }
---
+++ webapp/src/app/professional/professional.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProfessionalRoutingModule } from './professional-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ProfessionalRoutingModule
+  ],
+  declarations: []
+})
+export class ProfessionalModule { }
