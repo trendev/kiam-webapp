c8fe668605b6dcc2c1297c190582aac0300a0cd1
jsie <julien.sie@gmail.com>
2017-10-27 16:28:39 +0200

    Angular Material PreReqs

--- webapp/src/app/shared/shared.module.ts
+++ webapp/src/app/shared/shared.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   declarations: [PageNotFoundComponent],
   exports: [PageNotFoundComponent]
 })
+
 export class SharedModule { }
