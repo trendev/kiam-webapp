dd846df6c5b95db9fa43cd4c0c46d5094e3dfee6
jsie <julien.sie@gmail.com>
2017-10-31 23:58:44 +0100

    dynamic load of twitter widgets in RegisterForm - tmp

--- webapp/src/app/login/register-form/register-form.component.ts
+++ webapp/src/app/login/register-form/register-form.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.scss']
 })
-export class RegisterFormComponent implements OnInit {
 
-  constructor() { }
+export class RegisterFormComponent implements OnInit, AfterViewInit {
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    !function (d, s, id) {
+      var js: any,
+        fjs = d.getElementsByTagName(s)[0],
+        p = 'https';
+      if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + "://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    }
+      (document, "script", "twitter-wjs");
   }
 
 }
