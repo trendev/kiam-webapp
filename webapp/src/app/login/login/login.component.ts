2377954d851127702c29be558a6ccebb97453b38
jsie <julien.sie@gmail.com>
2017-11-02 22:33:19 +0100

    refactor implementing redirect()

--- webapp/src/app/login/login/login.component.ts
+++ webapp/src/app/login/login/login.component.ts
@@ -1,5 +1,5 @@
 import { environment } from '@env/environment';
-import { UserAccountType } from '@app/entities/user-account.model';
+import { UserAccountType, UserAccount } from '@app/entities';
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '@app/core';
 import { Router } from '@angular/router';
@@ -11,43 +11,52 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  username: string;
-  password: string;
-  message: string;
-
-  constructor(private authenticationService: AuthenticationService,
-    private router: Router) { }
-
   readonly main_title = `${environment.title}`;
   readonly title = `Identification à ${this.main_title}`;
   readonly subtitle = `Une identification est requise pour pouvoir utiliser les services sécurisés de ${this.main_title}`;
 
+  username: string;
+  password: string;
+  message: string;
+
+  user: UserAccount;
+
+  constructor(private authenticationService: AuthenticationService,
+    private router: Router) { }
+
   ngOnInit() {
+
+    this.user = this.authenticationService.user;
+    if (this.user) {
+
+    }
+  }
+
+  private redirect() {
+    let redirect: string;
+    switch (this.authenticationService.user.cltype) {
+      case UserAccountType.PROFESSIONAL:
+        redirect = this.authenticationService.redirectUrl ? this.authenticationService.redirectUrl : '/professional';
+        this.router.navigate([redirect]);
+        break;
+      case UserAccountType.INDIVIDUAL:
+        redirect = this.authenticationService.redirectUrl ? this.authenticationService.redirectUrl : '/individual';
+        this.router.navigate([redirect]);
+        break;
+      case UserAccountType.ADMINISTRATOR:
+        redirect = this.authenticationService.redirectUrl ? this.authenticationService.redirectUrl : '/administrator';
+        this.router.navigate([redirect]);
+        break;
+      default:
+        console.error(this.authenticationService.user.cltype + ' is not a supported type of UserAccount');
+        break;
+    }
   }
 
   login() {
     this.authenticationService.login(this.username, this.password)
       .subscribe(
-      auth => {
-        let redirect: string;
-        switch (this.authenticationService.user.cltype) {
-          case UserAccountType.PROFESSIONAL:
-            redirect = this.authenticationService.redirectUrl ? this.authenticationService.redirectUrl : '/professional';
-            this.router.navigate([redirect]);
-            break;
-          case UserAccountType.INDIVIDUAL:
-            redirect = this.authenticationService.redirectUrl ? this.authenticationService.redirectUrl : '/individual';
-            this.router.navigate([redirect]);
-            break;
-          case UserAccountType.ADMINISTRATOR:
-            redirect = this.authenticationService.redirectUrl ? this.authenticationService.redirectUrl : '/administrator';
-            this.router.navigate([redirect]);
-            break;
-          default:
-            console.error(this.authenticationService.user.cltype + ' is not a supported type of UserAccount');
-            break;
-        }
-      },
+      r => this.redirect(),
       e => this.message = 'Identification incorrecte : vérifier vos identifiants ou votre connexion au serveur'
       );
   }
