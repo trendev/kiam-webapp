8a1dc093d31054e93c50107a23b3d7205ba70c04
jsie <julien.sie@gmail.com>
2017-11-03 11:36:02 +0100

    inits the LoginGuard

--- webapp/src/app/login/login.module.ts
+++ webapp/src/app/login/login.module.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
+import { LoginGuard } from './login.guard';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { RegisterFormComponent } from './register-form/register-form.component';
     LoginRoutingModule
   ],
   declarations: [LoginComponent, RegisterFormComponent],
-  exports: [LoginComponent, RegisterFormComponent]
+  exports: [LoginComponent, RegisterFormComponent],
+  providers: [LoginGuard]
 })
 export class LoginModule { }
