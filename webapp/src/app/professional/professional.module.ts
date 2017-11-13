0e2d9b1748330f4deb04be4ff66693080a9e200b
jsie <julien.sie@gmail.com>
2017-11-13 17:21:18 +0100

    removes imports for eager loading

--- webapp/src/app/professional/professional.module.ts
+++ webapp/src/app/professional/professional.module.ts
@@ -6,12 +6,10 @@ import { ProfessionalDashboardComponent } from './professional-dashboard/profess
 import { ProfessionalGuard } from './professional.guard';
 import { SharedModule } from '@app/shared';
 import { ProfessionalToolbarComponent } from './professional-dashboard/professional-toolbar/professional-toolbar.component';
-import { WelcomeModule } from './welcome/welcome.module';
 
 @NgModule({
   imports: [
     SharedModule,
-    // WelcomeModule, // must be imported before ProfessionalRoutingModule or the module's routes won't be caught
     ProfessionalRoutingModule,
   ],
   declarations: [ProfessionalDashboardComponent, ProfessionalToolbarComponent],
