800d0ebac4db8bf412a5556e9789d9b788d393cd
jsie <julien.sie@gmail.com>
2017-11-14 21:04:20 +0100

    UnsupportedUserTypeComponent: fixes test issues

--- webapp/src/app/login/unsupported-user-type/unsupported-user-type.component.spec.ts
+++ webapp/src/app/login/unsupported-user-type/unsupported-user-type.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UnsupportedUserTypeComponent } from './unsupported-user-type.component';
+import { SharedModule } from '@app/shared';
+import { AuthenticationService, CoreModule } from '@app/core';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('UnsupportedUserTypeComponent', () => {
   let component: UnsupportedUserTypeComponent;
@@ -8,9 +12,13 @@ describe('UnsupportedUserTypeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UnsupportedUserTypeComponent ]
+      imports: [SharedModule, CoreModule, AppRoutingModule],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        AuthenticationService],
+      declarations: [UnsupportedUserTypeComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
