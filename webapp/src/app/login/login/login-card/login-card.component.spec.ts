7d98f9b7d15f0f9897b22f624d513edd3c181322
jsie <julien.sie@gmail.com>
2017-11-14 20:54:54 +0100

    LoginCardComponent: fixes test issues

--- webapp/src/app/login/login/login-card/login-card.component.spec.ts
+++ webapp/src/app/login/login/login-card/login-card.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginCardComponent } from './login-card.component';
+import { SharedModule } from '@app/shared';
+import { CoreModule, AuthenticationService } from '@app/core';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { DispatcherService } from '@app/login/dispatcher.service';
+import { APP_BASE_HREF } from '@angular/common';
+import { CredentialsManagerService } from '@app/login/credentials-manager.service';
+import { AppModule } from '@app/app.module';
 
 describe('LoginCardComponent', () => {
   let component: LoginCardComponent;
@@ -8,9 +15,16 @@ describe('LoginCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginCardComponent ]
+      imports: [
+        SharedModule, CoreModule, AppRoutingModule, AppModule
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        AuthenticationService,
+        DispatcherService,
+        CredentialsManagerService]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
