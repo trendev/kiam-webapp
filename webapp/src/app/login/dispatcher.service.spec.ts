8de6ababc749f9e5db4ea2d9961595fdc6d4f033
jsie <julien.sie@gmail.com>
2017-11-14 20:44:30 +0100

    DispatcherService: fixes test issues

--- webapp/src/app/login/dispatcher.service.spec.ts
+++ webapp/src/app/login/dispatcher.service.spec.ts
@@ -1,11 +1,23 @@
+import { AuthenticationService, CoreModule } from '@app/core';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { DispatcherService } from './dispatcher.service';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { SharedModule } from '@app/shared';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('DispatcherService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DispatcherService]
+      imports: [
+        CoreModule,
+        SharedModule,
+        AppRoutingModule
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        AuthenticationService,
+        DispatcherService]
     });
   });
 
