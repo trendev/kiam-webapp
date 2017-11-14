404cf70a3d640e8fe5121988095c7262c719ecf7
jsie <julien.sie@gmail.com>
2017-11-14 23:00:51 +0100

    implement/test BusinessService

--- webapp/src/app/core/business.service.spec.ts
+++ webapp/src/app/core/business.service.spec.ts
@@ -1,15 +1,54 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, async } from '@angular/core/testing';
 
 import { BusinessService } from './business.service';
+import { CoreModule } from './core.module';
+import { AuthenticationService } from './authentication.service';
+import { Subscription } from 'rxjs/Subscription';
+import { UserAccountType, Business } from '@app/entities';
 
 describe('BusinessService', () => {
+
+  const username = 'vanessa.gay@gmail.com';
+  const password = 'ponpon';
+
+  let loginSuscription: Subscription;
+  let logoutSuscription: Subscription;
+  let subscription: Subscription;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [CoreModule],
       providers: [BusinessService]
     });
   });
 
-  it('should be created', inject([BusinessService], (service: BusinessService) => {
+  beforeEach(async(inject([AuthenticationService],
+    (authService: AuthenticationService) => {
+      loginSuscription = authService.login(username, password).subscribe(result => { });
+    })));
+
+  afterEach(async(inject([AuthenticationService],
+    (authService: AuthenticationService) => {
+      logoutSuscription = authService.logout().subscribe(result => { });
+    })));
+
+  afterEach(() => {
+    if (loginSuscription && !loginSuscription.closed) {
+      loginSuscription.unsubscribe();
+      expect(loginSuscription.closed).toBeTruthy('subscription should be closed');
+    }
+    if (logoutSuscription && !logoutSuscription.closed) {
+      logoutSuscription.unsubscribe();
+      expect(logoutSuscription.closed).toBeTruthy('subscription should be closed');
+    }
+  });
+
+  it('should be created', async(inject([BusinessService], (service: BusinessService) => {
     expect(service).toBeTruthy();
-  }));
+    subscription = service.businesses.subscribe(
+      result => {
+        expect(result.length).toBeGreaterThanOrEqual(3, 'result.length should be 3');
+        expect(result.filter(b => b instanceof Business).length).toBeGreaterThanOrEqual(3, 'result should contains Business objects');
+      });
+  })));
 });
--- webapp/src/app/core/business.service.ts
+++ webapp/src/app/core/business.service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Optional } from '@angular/core';
 import { environment } from '@env/environment';
+import { HttpClient } from '@angular/common/http';
+import { ErrorHandlerService } from './error-handler.service';
+import { Observable } from 'rxjs/Observable';
+import { switchMap } from 'rxjs/operators';
+
+import { Business } from '@app/entities';
 
 @Injectable()
 export class BusinessService {
 
   readonly api = `${environment.api}/Business`;
 
-  constructor() { }
+  private _businesses: Business[];
+
+  constructor(private http: HttpClient, private errorHandler: ErrorHandlerService) { }
+
+  get businesses(): Observable<Business[]> {
+
+    if (this._businesses !== undefined) {
+      return Observable.of(this._businesses);
+    } else {
+      return this.http.get<Business[]>(`${this.api}`,
+        {
+          withCredentials: true
+        })
+        .map(result => {
+          this._businesses = result.map(r => new Business(r));
+          return this._businesses;
+        })
+        .catch(e => this.errorHandler.handle(e));
+    }
+  }
 
 }
