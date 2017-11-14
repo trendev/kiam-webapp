75efac198ab1c72b83fa1af2dedea09956bc3c7e
jsie <julien.sie@gmail.com>
2017-11-14 20:00:07 +0100

    AdministratorComponent: fixes test issues

--- webapp/src/app/administrator/administrator/administrator.component.spec.ts
+++ webapp/src/app/administrator/administrator/administrator.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdministratorComponent } from './administrator.component';
+import { SharedModule } from '@app/shared';
+import { CoreModule, AuthenticationService } from '@app/core';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AdministratorModule } from '@app/administrator/administrator.module';
+import { APP_BASE_HREF } from '@angular/common';
+
 
 describe('AdministratorComponent', () => {
   let component: AdministratorComponent;
@@ -8,9 +14,17 @@ describe('AdministratorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdministratorComponent ]
+      imports: [
+        CoreModule,
+        AdministratorModule,
+        AppRoutingModule
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        AuthenticationService
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
