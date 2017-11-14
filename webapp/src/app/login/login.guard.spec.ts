634ddb47e3b4c2b06ec913b276e1e7fcdce7cb03
jsie <julien.sie@gmail.com>
2017-11-14 20:47:27 +0100

    LoginGuard: fixes test issues

--- webapp/src/app/login/login.guard.spec.ts
+++ webapp/src/app/login/login.guard.spec.ts
@@ -2,10 +2,21 @@ import { TestBed, async, inject } from '@angular/core/testing';
 
 import { LoginGuard } from './login.guard';
 
+import { AuthenticationService, CoreModule } from '@app/core';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { SharedModule } from '@app/shared';
+import { APP_BASE_HREF } from '@angular/common';
+import { DispatcherService } from '@app/login/dispatcher.service';
+
 describe('LoginGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [LoginGuard]
+      imports: [CoreModule, AppRoutingModule, SharedModule],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        AuthenticationService,
+        DispatcherService,
+        LoginGuard]
     });
   });
 
