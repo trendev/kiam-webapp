4d4f55e8a86dcc61ac27b84741235655f4ae8f66
jsie <julien.sie@gmail.com>
2017-11-14 20:49:50 +0100

    ProfessionalGuard: fixes test issues

--- webapp/src/app/professional/professional.guard.spec.ts
+++ webapp/src/app/professional/professional.guard.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { ProfessionalGuard } from './professional.guard';
+import { AuthenticationService, CoreModule } from '@app/core';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { SharedModule } from '@app/shared';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('ProfessionalGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ProfessionalGuard]
+      imports: [CoreModule, AppRoutingModule, SharedModule],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        ProfessionalGuard,
+        AuthenticationService]
     });
   });
 
