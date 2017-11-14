fe76e43d5d0c4892f603c7b4bd2ea5361915c2e9
jsie <julien.sie@gmail.com>
2017-11-14 21:01:18 +0100

    RegisterFormComponent: fixes test issues

--- webapp/src/app/login/register-form/register-form.component.spec.ts
+++ webapp/src/app/login/register-form/register-form.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterFormComponent } from './register-form.component';
+import { SharedModule } from '@app/shared';
+import { LoginModule } from '@app/login';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('RegisterFormComponent', () => {
   let component: RegisterFormComponent;
@@ -8,9 +12,12 @@ describe('RegisterFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterFormComponent ]
+      imports: [SharedModule, LoginModule, AppRoutingModule],
+      providers: [
+        { provide: APP_BASE_HREF, useValue : '/' }
+    ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
