102a26ae65793e8f1956074dc707dfdcdcfb1bfb
jsie <julien.sie@gmail.com>
2017-11-14 19:51:37 +0100

    AdministratorGuard: fixes test issues

--- webapp/src/app/administrator/administrator.guard.spec.ts
+++ webapp/src/app/administrator/administrator.guard.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { AdministratorGuard } from './administrator.guard';
+import { AuthenticationService, CoreModule } from '@app/core';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { SharedModule } from '@app/shared';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('AdministratorGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AdministratorGuard]
+      imports: [CoreModule, AppRoutingModule, SharedModule],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        AdministratorGuard,
+        AuthenticationService]
     });
   });
 
