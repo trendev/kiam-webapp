3206f539c2b43df1696959a2c054967521509fed
jsie <julien.sie@gmail.com>
2017-11-20 20:07:27 +0100

    implements payment-mode.service

--- webapp/src/app/core/payment-mode.service.ts
+++ webapp/src/app/core/payment-mode.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
+import { environment } from '@env/environment';
+import { PaymentMode } from '@app/entities';
+import { HttpClient } from '@angular/common/http';
+import { ErrorHandlerService } from '@app/core';
+import { Observable } from 'rxjs/Observable';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class PaymentModeService {
 
-  constructor() { }
+  readonly api = `${environment.api}/PaymentMode`;
+
+  private _paymentModes: PaymentMode[];
+
+  constructor(private http: HttpClient, private errorHandler: ErrorHandlerService) { }
+
+  get paymentModes(): Observable<PaymentMode[]> {
+
+    if (this._paymentModes !== undefined) {
+      return Observable.of(this._paymentModes);
+    } else {
+      return this.http.get<PaymentMode[]>(`${this.api}`,
+        {
+          withCredentials: true
+        })
+        .map(result => {
+          this._paymentModes = result.map(r => new PaymentMode(r));
+          return this._paymentModes;
+        })
+        .catch(e => this.errorHandler.handle(e));
+    }
+
+  }
 
 }
