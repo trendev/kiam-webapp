04756ff2d325c252020e361f3ed1fb5484df8665
jsie <julien.sie@gmail.com>
2017-11-30 13:51:29 +0100

    improves AuthenticationService

--- webapp/src/app/core/authentication.service.spec.ts
+++ webapp/src/app/core/authentication.service.spec.ts
@@ -17,6 +17,7 @@ describe('AuthService', () => {
 
   const username = 'vanessa.gay@gmail.com';
   const password = 'ponpon';
+  let spy: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -32,6 +33,14 @@ describe('AuthService', () => {
     expect(service.user).toBeUndefined();
     expect(service.redirectUrl).toBeUndefined();
     expect(service.api).toBe(`${environment.api}/Authentication`);
+  }));
+
+  it('should reset the user and _isLoggedIn', inject([AuthenticationService], (service: AuthenticationService) => {
+    spy = spyOn(service, 'reset').and.callThrough();
+    service.reset();
+    expect(service.reset).toHaveBeenCalled();
+    expect(service.user).toBeUndefined();
+    expect(service.isLoggedIn).toBeFalsy();
   }));
 
   it('should login ' + username, async(inject([AuthenticationService], (service: AuthenticationService) => {
@@ -56,11 +65,39 @@ describe('AuthService', () => {
     });
   })));
 
-  it('should get a password', async(inject([AuthenticationService], (service: AuthenticationService) => {
+  it('should get a password, size = 100', async(inject([AuthenticationService], (service: AuthenticationService) => {
     expect(service).toBeTruthy();
     const size = 100;
     service.password(size).subscribe(pwd => {
       expect(pwd.length).toBe(size);
+    });
+  })));
+
+  it('should get a password, default size (=10)', async(inject([AuthenticationService], (service: AuthenticationService) => {
+    expect(service).toBeTruthy();
+    const size = 10;
+    service.password().subscribe(pwd => {
+      expect(pwd.length).toBe(size);
+    });
+  })));
+
+  it('should set a new password', async(inject([AuthenticationService], (service: AuthenticationService) => {
+    expect(service).toBeTruthy();
+    const pwd = 'comptandye_password';
+    const pwdHash = '8oOK6BsXsv+s19XMaKMUYh6y0IvVf4Kjb7GmSWS/M0Y=';
+
+    service.newPassword(pwd).subscribe(epwd => {
+      expect(epwd).toBe(pwdHash);
+    });
+  })));
+
+  it('should reset the password', async(inject([AuthenticationService], (service: AuthenticationService) => {
+    expect(service).toBeTruthy();
+    const pwd = password;
+    const pwdHash = 'EUrVrX4nfmYYFxpMyRX93OlkJxNZv9mkMGfirZKbhWI=';
+
+    service.newPassword(pwd).subscribe(epwd => {
+      expect(epwd).toBe(pwdHash);
     });
   })));
 
