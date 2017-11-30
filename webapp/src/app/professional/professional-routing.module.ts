b61400fbd59a04c36a95edaa3548813b8a4391b2
jsie <julien.sie@gmail.com>
2017-11-30 11:23:12 +0100

    change default route to clients

--- webapp/src/app/professional/professional-routing.module.ts
+++ webapp/src/app/professional/professional-routing.module.ts
@@ -45,7 +45,7 @@ const routes: Routes = [
         path: 'offerings',
         loadChildren: 'app/professional/offerings/offerings.module#OfferingsModule'
       },
-      { path: '', redirectTo: 'profile', pathMatch: 'full' },
+      { path: '', redirectTo: 'clients', pathMatch: 'full' },
       { path: '**', component: PageNotFoundComponent }
     ]
   }
