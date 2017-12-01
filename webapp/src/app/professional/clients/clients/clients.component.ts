f8f63657613052a352969d8d7301ea1737d5a124
jsie <julien.sie@gmail.com>
2017-12-01 18:50:01 +0100

    inits the clients list

--- webapp/src/app/professional/clients/clients/clients.component.ts
+++ webapp/src/app/professional/clients/clients/clients.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 import { ProfessionalService } from '@app/core';
 import { Client } from '@app/entities';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-clients',
@@ -14,16 +15,21 @@ export class ClientsComponent implements OnInit {
   constructor(private professionalService: ProfessionalService) { }
 
   clients: Client[] = [];
+  displayedColumns = ['lastName', 'firstName', 'phone'];
+  dataSource;
 
   ngOnInit() {
     this.professionalService.clients.subscribe(
-      clients => this.clients = clients.sort(
-        (cl1, cl2) => {
-          const lastName1 = cl1.customerDetails.lastName || '';
-          const lastName2 = cl2.customerDetails.lastName || '';
-          return lastName1.localeCompare(lastName2);
-        }
-      ),
+      clients => {
+        this.clients = clients.sort(
+          (cl1, cl2) => {
+            const lastName1 = cl1.customerDetails.lastName || '';
+            const lastName2 = cl2.customerDetails.lastName || '';
+            return lastName1.localeCompare(lastName2);
+          }
+        );
+        this.dataSource = new MatTableDataSource<Client>(this.clients);
+      },
       // TODO : handle the error
       e => console.error(`Une erreur est survenue lors de la collecte des clients sur le serveur`)
     );
