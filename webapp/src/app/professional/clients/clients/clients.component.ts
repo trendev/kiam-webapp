f824dd99e1f2e0daf81f55f00e7cb4d8617d2443
jsie <julien.sie@gmail.com>
2017-12-01 18:18:59 +0100

    sort the client in inits

--- webapp/src/app/professional/clients/clients/clients.component.ts
+++ webapp/src/app/professional/clients/clients/clients.component.ts
@@ -13,11 +13,17 @@ export class ClientsComponent implements OnInit {
 
   constructor(private professionalService: ProfessionalService) { }
 
-  clients: Client[];
+  clients: Client[] = [];
 
   ngOnInit() {
     this.professionalService.clients.subscribe(
-      clients => this.clients = clients,
+      clients => this.clients = clients.sort(
+        (cl1, cl2) => {
+          const lastName1 = cl1.customerDetails.lastName || '';
+          const lastName2 = cl2.customerDetails.lastName || '';
+          return lastName1.localeCompare(lastName2);
+        }
+      ),
       // TODO : handle the error
       e => console.error(`Une erreur est survenue lors de la collecte des clients sur le serveur`)
     );
