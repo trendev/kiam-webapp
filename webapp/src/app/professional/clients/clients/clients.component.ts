9980afa1bef53a025c32371d0c95ad00c7c9996c
jsie <julien.sie@gmail.com>
2017-12-02 15:08:47 +0100

    dynamic columns

--- webapp/src/app/professional/clients/clients/clients.component.ts
+++ webapp/src/app/professional/clients/clients/clients.component.ts
@@ -15,24 +15,44 @@ export class ClientsComponent implements OnInit {
   constructor(private professionalService: ProfessionalService) { }
 
   clients: Client[] = [];
-  displayedColumns = ['lastName', 'firstName', 'phone'];
+
+  displayedColumns;
   dataSource;
 
-  ngOnInit() {
-    this.professionalService.clients.subscribe(
-      clients => {
-        this.clients = clients.sort(
-          (cl1, cl2) => {
-            const lastName1 = cl1.customerDetails.lastName || '';
-            const lastName2 = cl2.customerDetails.lastName || '';
-            return lastName1.localeCompare(lastName2);
-          }
-        );
-        this.dataSource = new MatTableDataSource<Client>(this.clients);
-      },
-      // TODO : handle the error
-      e => console.error(`Une erreur est survenue lors de la collecte des clients sur le serveur`)
-    );
-  }
+  columns: ClientColumnDef[] = [
+    { columnDef: 'id', headerCellDef: 'Code client', cellDef: (client: Client) => client.id, hide: true },
+    { columnDef: 'lastName', headerCellDef: 'Nom', cellDef: (client: Client) => client.customerDetails.lastName, hide: false },
+    { columnDef: 'firstName', headerCellDef: 'Prénom', cellDef: (client: Client) => client.customerDetails.firstName, hide: false },
+    { columnDef: 'nickname', headerCellDef: 'Surnom', cellDef: (client: Client) => client.customerDetails.nickname, hide: true },
+    { columnDef: 'phone', headerCellDef: 'Tél.', cellDef: (client: Client) => client.customerDetails.phone, hide: true },
+    { columnDef: 'email', headerCellDef: 'Email', cellDef: (client: Client) => client.email, hide: true },
+  ];
 
+ngOnInit() {
+
+  this.displayedColumns = this.columns.map(c => c.columnDef);
+
+  this.professionalService.clients.subscribe(
+    clients => {
+      this.clients = clients.sort(
+        (cl1, cl2) => {
+          const lastName1 = cl1.customerDetails.lastName || '';
+          const lastName2 = cl2.customerDetails.lastName || '';
+          return lastName1.localeCompare(lastName2);
+        }
+      );
+      this.dataSource = new MatTableDataSource<Client>(this.clients);
+    },
+    // TODO : handle the error
+    e => console.error(`Une erreur est survenue lors de la collecte des clients sur le serveur`)
+  );
+}
+
+}
+
+interface ClientColumnDef {
+  columnDef: string;
+  headerCellDef: string;
+  cellDef: (client: Client) => any;
+  hide: boolean;
 }
