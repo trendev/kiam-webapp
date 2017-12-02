c452d13b9888b40afdd19d103bb00395cb2909b1
jsie <julien.sie@gmail.com>
2017-12-02 19:47:43 +0100

    sort the data (using a datamodel)

--- webapp/src/app/professional/clients/clients/clients.component.ts
+++ webapp/src/app/professional/clients/clients/clients.component.ts
@@ -1,58 +1,76 @@
 import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { ProfessionalService } from '@app/core';
 import { Client } from '@app/entities';
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource, MatSort, Sort } from '@angular/material';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.scss'],
 })
-export class ClientsComponent implements OnInit {
+export class ClientsComponent implements OnInit, AfterViewInit {
 
   constructor(private professionalService: ProfessionalService) { }
 
-  clients: Client[] = [];
+  clients: ClientModel[] = [];
 
-  displayedColumns;
-  dataSource;
+  displayedColumns: string[];
+  datasource: MatTableDataSource<ClientModel>;
 
   columns: ClientColumnDef[] = [
-    { columnDef: 'id', headerCellDef: 'Code client', cellDef: (client: Client) => client.id, hide: true },
-    { columnDef: 'lastName', headerCellDef: 'Nom', cellDef: (client: Client) => client.customerDetails.lastName, hide: false },
-    { columnDef: 'firstName', headerCellDef: 'Prénom', cellDef: (client: Client) => client.customerDetails.firstName, hide: false },
-    { columnDef: 'nickname', headerCellDef: 'Surnom', cellDef: (client: Client) => client.customerDetails.nickname, hide: true },
-    { columnDef: 'phone', headerCellDef: 'Tél.', cellDef: (client: Client) => client.customerDetails.phone, hide: true },
-    { columnDef: 'email', headerCellDef: 'Email', cellDef: (client: Client) => client.email, hide: true },
+    { columnDef: 'id', headerCellDef: 'Code client', cellDef: (client: ClientModel) => client.id, hide: true },
+    { columnDef: 'lastName', headerCellDef: 'Nom', cellDef: (client: ClientModel) => client.lastName, hide: false },
+    { columnDef: 'firstName', headerCellDef: 'Prénom', cellDef: (client: ClientModel) => client.firstName, hide: false },
+    { columnDef: 'nickname', headerCellDef: 'Surnom', cellDef: (client: ClientModel) => client.nickname, hide: true },
+    { columnDef: 'phone', headerCellDef: 'Tél.', cellDef: (client: ClientModel) => client.phone, hide: true },
+    { columnDef: 'email', headerCellDef: 'Email', cellDef: (client: ClientModel) => client.email, hide: true },
   ];
 
-ngOnInit() {
+  @ViewChild(MatSort) sort: MatSort;
 
-  this.displayedColumns = this.columns.map(c => c.columnDef);
+  ngOnInit() {
+    this.displayedColumns = this.columns.map(c => c.columnDef);
+  }
 
-  this.professionalService.clients.subscribe(
-    clients => {
-      this.clients = clients.sort(
-        (cl1, cl2) => {
-          const lastName1 = cl1.customerDetails.lastName || '';
-          const lastName2 = cl2.customerDetails.lastName || '';
-          return lastName1.localeCompare(lastName2);
-        }
-      );
-      this.dataSource = new MatTableDataSource<Client>(this.clients);
-    },
-    // TODO : handle the error
-    e => console.error(`Une erreur est survenue lors de la collecte des clients sur le serveur`)
-  );
+  ngAfterViewInit() {
+    this.professionalService.clients.subscribe(
+      clients => {
+        this.clients = clients.map(client => {
+          return {
+            id: client.id + '',
+            lastName: client.customerDetails.lastName || '',
+            firstName: client.customerDetails.firstName || '',
+            nickname: client.customerDetails.nickname || '',
+            phone: client.customerDetails.phone || '',
+            email: client.email || ''
+          };
+        });
+        this.datasource = new MatTableDataSource<ClientModel>(this.clients);
+        this.datasource.sort = this.sort;
+      },
+      // TODO : handle the error
+      e => console.error(`Une erreur est survenue lors de la collecte des clients sur le serveur`)
+    );
+  }
+
 }
 
+interface ClientModel {
+  id: string;
+  lastName: string;
+  firstName: string;
+  nickname: string;
+  phone: string;
+  email: string;
 }
 
 interface ClientColumnDef {
   columnDef: string;
   headerCellDef: string;
-  cellDef: (client: Client) => any;
+  cellDef: (client: ClientModel) => any;
   hide: boolean;
 }
+
+
