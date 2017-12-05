1c980c0b44f8492a604279d3f603db7cc6be16d6
jsie <julien.sie@gmail.com>
2017-12-05 19:55:54 +0100

    connect collective-groups in create-client

--- webapp/src/app/shared/collective-groups/collective-groups.component.ts
+++ webapp/src/app/shared/collective-groups/collective-groups.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ControlContainer, FormGroupDirective, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-collective-groups',
   templateUrl: './collective-groups.component.html',
-  styleUrls: ['./collective-groups.component.scss']
+  styleUrls: ['./collective-groups.component.scss'],
+  viewProviders: [
+    {
+      provide: ControlContainer,
+      useExisting: FormGroupDirective
+    }
+  ]
 })
 export class CollectiveGroupsComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(private parent: FormGroupDirective) { }
 
   ngOnInit() {
+    if (this.parent.form === null) {
+      throw new Error(`parent: FormGroupDirective should not be null in CollectiveGroupsComponent#init()`);
+    }
+    this.form = this.parent.form;
+  }
+
+  get collectiveGroups(): FormArray {
+    return this.form.get('collectiveGroups') as FormArray;
   }
 
 }
