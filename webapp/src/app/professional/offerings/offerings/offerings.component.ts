803747f8852014d846ebbe3dff92f450186f5d84
jsie <julien.sie@gmail.com>
2017-12-15 16:03:34 +0100

    radio button choice

--- webapp/src/app/professional/offerings/offerings/offerings.component.ts
+++ webapp/src/app/professional/offerings/offerings/offerings.component.ts
@@ -13,7 +13,17 @@ export class OfferingsComponent implements OnInit {
   private _offerings: Offering[];
   services: Service[] = [];
   packs: Pack[] = [];
-
+  _selectedOfferingType = OfferingType.SERVICE;
+  offeringsTypes = [
+    {
+      type: OfferingType.SERVICE,
+      label: 'Service'
+    },
+    {
+      type: OfferingType.PACK,
+      label: 'Forfait'
+    }
+  ];
 
   constructor(private professionalService: ProfessionalService,
     private router: Router,
@@ -43,11 +53,20 @@ export class OfferingsComponent implements OnInit {
             throw new TypeError(`initOfferings(): ${o.cltype} is not a supported offering type`);
         }
       });
-      this._offerings = []; // reset the _offerings, not used after initOfferings()
+    this._offerings = []; // reset the _offerings, not used after initOfferings()
   }
 
   ngOnInit() {
     this.initOfferings();
   }
+
+  get selectedOfferingType(): string {
+    return this._selectedOfferingType;
+  }
+
+  set selectedOfferingType(value: string){
+    this._selectedOfferingType = value;
+  }
+
 
 }
