6106836911f0ba37d2d1a617b60957fd6411ee26
jsie <julien.sie@gmail.com>
2017-12-17 15:13:51 +0100

    inits pack-detail

--- webapp/src/app/professional/offerings/offerings.module.ts
+++ webapp/src/app/professional/offerings/offerings.module.ts
@@ -5,12 +5,13 @@ import { SharedModule } from '@app/shared';
 import { OfferingsComponent } from './offerings/offerings.component';
 import { ServicesComponentComponent } from './services-component/services-component.component';
 import { PacksComponentComponent } from './packs-component/packs-component.component';
+import { PackDetailComponent } from './pack-detail/pack-detail.component';
 
 @NgModule({
   imports: [
     SharedModule,
     OfferingsRoutingModule
   ],
-  declarations: [OfferingsComponent, ServicesComponentComponent, PacksComponentComponent]
+  declarations: [OfferingsComponent, ServicesComponentComponent, PacksComponentComponent, PackDetailComponent]
 })
 export class OfferingsModule { }
---
+++ webapp/src/app/professional/offerings/pack-detail/pack-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pack-detail',
+  templateUrl: './pack-detail.component.html',
+  styleUrls: ['./pack-detail.component.scss']
+})
+export class PackDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
