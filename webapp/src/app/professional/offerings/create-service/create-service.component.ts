88cf3236709e349fbd900b211a7b887a092a3bb7
jsie <julien.sie@gmail.com>
2017-12-21 21:44:21 +0100

    CreateServiceComponent

--- webapp/src/app/professional/offerings/create-service/create-service.component.ts
+++ webapp/src/app/professional/offerings/create-service/create-service.component.ts
@@ -1,15 +1,104 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Business, OfferingType, Service } from '@app/entities';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ErrorAggregatorDirective, CustomValidators, Utils, compareBusinessesFn } from '@app/shared';
+import { ServiceService } from '@app/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-create-service',
   templateUrl: './create-service.component.html',
   styleUrls: ['./create-service.component.scss']
 })
-export class CreateServiceComponent implements OnInit {
+export class CreateServiceComponent {
 
-  constructor() { }
+  professionalBusinesses: Business[];
 
-  ngOnInit() {
+  form: FormGroup;
+
+  readonly ot = OfferingType.SERVICE;
+
+  @ViewChild(ErrorAggregatorDirective) errorAggregator: ErrorAggregatorDirective;
+
+  constructor(private fb: FormBuilder,
+    private serviceService: ServiceService,
+    private router: Router,
+    private route: ActivatedRoute) {
+    this.route.data.subscribe(
+      (data: {
+        businesses: Business[]
+      }) => {
+        this.professionalBusinesses = data.businesses;
+
+        this.form = this.createForm();
+
+        this.form.valueChanges.forEach(_ => {
+          if (this.form.invalid && this.errorAggregator) {
+            this.errorAggregator.viewContainerRef.clear();
+          }
+        });
+
+      });
   }
 
+  createForm(): FormGroup {
+    const fg = this.fb.group({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(75),
+        CustomValidators.blankStringForbidden
+      ]),
+      price: new FormControl(0, [
+        Validators.required,
+        Validators.min(0)
+      ]),
+      duration: new FormControl(1, [
+        Validators.required,
+        Validators.min(1)
+      ]),
+      businesses: this.fb.array([], CustomValidators.selectedElementRequired)
+    });
+
+    Utils.initFormArray(fg,
+      'businesses',
+      this.professionalBusinesses,
+      b => this.fb.group({
+        designation: b.designation,
+        value: false
+      }),
+      compareBusinessesFn);
+
+    return fg;
+  }
+
+  revert() {
+    // resets the form field based on the raw value
+    this.form.reset(this.createForm().getRawValue());
+  }
+
+  save() {
+    const service = this.prepareSave();
+    this.serviceService.create(service).subscribe(
+      _service => this.router.navigate(['../../', { ot: this.ot }], { relativeTo: this.route }),
+      // TODO: handle this (check the status code, etc)
+      e => console.error('Impossible de sauvegarder la prestation sur le serveur')
+    );
+  }
+
+  prepareSave(): Service {
+    const value = this.form.getRawValue();
+
+    const service = new Service({
+      name: value.name,
+      price: value.price * 100,
+      duration: value.duration,
+      businesses: Utils.extractArrayFromControl(this.form, 'businesses',
+        fg => new Business({
+          designation: fg.value.designation
+        })
+      )
+    });
+
+    return service;
+  }
 }
