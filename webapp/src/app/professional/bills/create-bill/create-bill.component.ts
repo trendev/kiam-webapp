3f9dc7d9989831d2ecc290307e53238833f5d538
jsie <julien.sie@gmail.com>
2017-12-26 17:10:31 +0100

    init the card

--- webapp/src/app/professional/bills/create-bill/create-bill.component.ts
+++ webapp/src/app/professional/bills/create-bill/create-bill.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Offering, PaymentMode } from '@app/entities';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-bill',
   templateUrl: './create-bill.component.html',
   styleUrls: ['./create-bill.component.scss']
 })
-export class CreateBillComponent implements OnInit {
+export class CreateBillComponent {
+  @Input() offerings: Offering[];
+  @Input() paymentModes: PaymentMode[];
+  @Input() id: number;
+  @Input() name: string;
+
+  form = new FormGroup({});
+
   constructor() { }
 
-  ngOnInit() {
+  revert() {
+
+  }
+
+  save() {
+
   }
 
 }
