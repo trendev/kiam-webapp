92434d82dbc86c47da0ba31919fb69bbafeee55c
jsie <julien.sie@gmail.com>
2017-12-26 16:00:07 +0100

    implement ProfessionalPaymentModesResolverService

--- webapp/src/app/core/professional-payment-modes-resolver.service.ts
+++ webapp/src/app/core/professional-payment-modes-resolver.service.ts
@@ -1,8 +1,15 @@
+import { ProfessionalService } from './professional.service';
 import { Injectable } from '@angular/core';
+import { PaymentMode } from '@app/entities';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class ProfessionalPaymentModesResolverService {
+export class ProfessionalPaymentModesResolverService implements Resolve<PaymentMode[]> {
+  constructor(private professionalService: ProfessionalService) { }
 
-  constructor() { }
-
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): PaymentMode[] | Observable<PaymentMode[]> | Promise<PaymentMode[]> {
+    return this.professionalService.profile()
+      .map(pro => pro.paymentModes ? pro.paymentModes : []);
+  }
 }
