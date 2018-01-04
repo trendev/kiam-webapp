3ccda6cb34337d4ea778cb5c61c50afee2246871
jsie <julien.sie@gmail.com>
2018-01-04 14:37:12 +0100

    ClientBillResolverService

--- webapp/src/app/professional/bills/client-bill-resolver.service.ts
+++ webapp/src/app/professional/bills/client-bill-resolver.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Resolve, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ClientBill } from '@app/entities';
+import { ClientService } from '@app/core';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class ClientBillResolverService {
+export class ClientBillResolverService implements Resolve<ClientBill> {
 
-  constructor() { }
+  constructor(private clientService: ClientService,
+    private router: Router) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): ClientBill | Observable<ClientBill> | Promise<ClientBill> {
+    const id = +route.paramMap.get('id');
+    const ref = route.paramMap.get('ref');
+    return this.clientService.getClientBill(id, ref)
+      .catch(e => {
+        this.router.navigate(['/professional/clients']);
+        return Observable.of(null);
+      });
+  }
 
 }
