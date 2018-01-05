b451cc1d6708e2d03d976e4ee66f35c262d8f6a1
jsie <julien.sie@gmail.com>
2018-01-05 16:37:43 +0100

    implement fixedpurchasedofferings

--- webapp/src/app/professional/bills/fixed-purchased-offerings/fixed-purchased-offerings.component.ts
+++ webapp/src/app/professional/bills/fixed-purchased-offerings/fixed-purchased-offerings.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { PurchasedOffering } from '@app/entities';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ControlContainer, FormGroupDirective, FormGroup, AbstractControl } from '@angular/forms';
+import { MatTableDataSource, MatSort } from '@angular/material';
+import { Utils } from '@app/shared';
 
 @Component({
   selector: 'app-fixed-purchased-offerings',
   templateUrl: './fixed-purchased-offerings.component.html',
-  styleUrls: ['./fixed-purchased-offerings.component.scss']
+  styleUrls: ['./fixed-purchased-offerings.component.scss'],
+  viewProviders: [
+    {
+      provide: ControlContainer,
+      useExisting: FormGroupDirective
+    }
+  ]
 })
 export class FixedPurchasedOfferingsComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  purchasedOfferingsModel: PurchasedOfferingModel[];
+  displayedColumns = [
+    'qty', 'name', 'price', 'businesses'];
+  datasource: MatTableDataSource<PurchasedOfferingModel>;
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  pomSortFn = (pom1, pom2) => pom1.name.localeCompare(pom2.name);
+
+  constructor(private parent: FormGroupDirective) { }
 
   ngOnInit() {
+    if (!this.parent.form) {
+      throw new Error(`PackContentComponent#ngOnInit(): this.parent form should not be undefined or null`);
+    }
+    this.form = this.parent.form;
+    this.initpurchasedOfferingsModel();
   }
 
+  get purchasedOfferingsContent(): AbstractControl {
+    return this.form.get('purchasedOfferings').get('content');
+  }
+
+  initpurchasedOfferingsModel() {
+    const purchasedOfferings = this.purchasedOfferingsContent.value as PurchasedOffering[];
+    this.purchasedOfferingsModel = purchasedOfferings
+      .map(
+      po => {
+        return {
+          qty: po.qty,
+          name: po.offering.name,
+          price: po.offering.price,
+          businesses: Utils.getBusinesses(po.offering.businesses) // display businesses as a string
+        };
+      }
+      )
+      .sort(this.pomSortFn);
+
+    this.datasource =
+      new MatTableDataSource<PurchasedOfferingModel>(this.purchasedOfferingsModel);
+    this.datasource.sort = this.sort;
+  }
+
+}
+
+interface PurchasedOfferingModel {
+  qty: number;
+  name: string;
+  price: number;
+  businesses: string;
 }
