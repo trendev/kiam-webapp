75d0646683210f672051e48c2e59c8956c65b418
jsie <julien.sie@gmail.com>
2018-01-09 17:14:58 +0100

    checkRouterEvent in AppComponent

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { UserAccount } from '@app/entities';
 import { AuthenticationService } from '@app/core';
-import { Router } from '@angular/router';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor() { }
+  constructor(private router: Router) {
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      console.log('Start');
+    }
+
+    if (routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError) {
+      console.log('End Cancel or Error');
+    }
+  }
 }
