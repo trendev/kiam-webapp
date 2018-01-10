64433840c97d5d5d26574d898adba571542a0ec3
jsie <julien.sie@gmail.com>
2018-01-10 17:15:45 +0100

    update professional toolbar with loading service

--- webapp/src/app/professional/professional-dashboard/professional-toolbar/professional-toolbar.component.ts
+++ webapp/src/app/professional/professional-dashboard/professional-toolbar/professional-toolbar.component.ts
@@ -2,6 +2,8 @@ import { Router, NavigationExtras } from '@angular/router';
 import { AuthenticationService } from '@app/core';
 import { environment } from '@env/environment';
 import { Component } from '@angular/core';
+import { LoadingOverlayService } from '@app/loading-overlay.service';
+import 'rxjs/add/operator/finally';
 
 @Component({
   selector: 'app-professional-toolbar',
@@ -13,12 +15,17 @@ export class ProfessionalToolbarComponent {
   title = `${environment.title}`;
 
   constructor(private authenticationService: AuthenticationService,
-    private router: Router) { }
+    private router: Router,
+    private loadingOverlayService: LoadingOverlayService) { }
 
   logout() {
-    this.authenticationService.logout().subscribe(
-      v => this.router.navigate(['/login'], this.authenticationService.loginRequired),
-      e => this.router.navigate(['/login'], this.authenticationService.loginRequired));
+    this.loadingOverlayService.start();
+    this.authenticationService.logout()
+      .finally(() => {
+        this.loadingOverlayService.stop();
+        this.router.navigate(['/login'], this.authenticationService.loginRequired);
+      })
+      .subscribe(); // TODO : handle success and failure
   }
 
 }
