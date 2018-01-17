5037d9d454bd29a173b4b774e02316377af53391
jsie <julien.sie@gmail.com>
2018-01-17 11:42:03 +0100

    updateInput

--- webapp/src/app/shared/phone/phone.component.ts
+++ webapp/src/app/shared/phone/phone.component.ts
@@ -136,11 +136,8 @@ export class PhoneComponent implements ControlValueAccessor, MatFormFieldControl
 
   registerOnChange(fn: any): void {
     this.onChange = (value: string) => {
-      // const start = this.input.nativeElement.selectionStart;
-      const val = Utils.formatPhoneNumber(value);
-      this.input.nativeElement.value = val;
-      // let diff = this.input.nativeElement.value.length - value.length;
-      // this.input.nativeElement.setSelectionRange(start + diff, start + diff);
+
+      this.updateInput(value);
 
       this._value = value;
       this.stateChanges.next();
@@ -151,6 +148,38 @@ export class PhoneComponent implements ControlValueAccessor, MatFormFieldControl
         fn(value);
       }
     };
+  }
+
+  private updateInput(inputValue: string) {
+    const start = this.input.nativeElement.selectionStart;
+    const newValue = Utils.formatPhoneNumber(inputValue);
+    this.input.nativeElement.value = newValue;
+
+    // compute the length diff between the provided value and the formated one
+    const diff = newValue.length - inputValue.length;
+
+    // caret's offset (no move by default)
+    let offset = start;
+
+    switch (diff) {
+      case -1: { // remove a character
+        if (/\s/.test(`${inputValue.charAt(start - 1)}`)) { // if original char is a whitespace
+          offset = start + diff; // slide to the left
+        }
+        break;
+      }
+      case 1: { // add a character
+        if (/\s/.test(`${newValue.charAt(start - 1)}`)) { // if new char is a whitespace
+          offset = start + diff; // slide to the right
+        }
+        break;
+      }
+      default: { // case for copy/paste
+        offset = start + diff;
+        break;
+      }
+    }
+    this.input.nativeElement.setSelectionRange(offset, offset);
   }
 
   registerOnTouched(fn: any): void {
