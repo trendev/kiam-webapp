d175859b840672344e6bbda1d61f99b63b2beb2c
jsie <julien.sie@gmail.com>
2018-01-20 18:46:16 +0100

    copy/paste clientbilllist to bills

--- webapp/src/app/professional/bills/bills/bills.component.ts
+++ webapp/src/app/professional/bills/bills/bills.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Bill, IndividualBill, ClientBill, CollectiveGroupBill, BillType } from '@app/entities';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Bill } from '@app/entities';
+import { MatTableDataSource, MatSort } from '@angular/material';
 import { ProfessionalService } from '@app/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { LoadingOverlayService } from '@app/loading-overlay.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-bills',
@@ -11,18 +13,18 @@ import { LoadingOverlayService } from '@app/loading-overlay.service';
 })
 export class BillsComponent implements OnInit {
 
-  private _bills: Bill[];
-  clientBills: ClientBill[] = [];
-  individualBills: IndividualBill[] = [];
-  collectiveGroupBills: CollectiveGroupBill[] = [];
+  private bills: Bill[];
+  billsModel: Bill[];
 
-  readonly billType = {
-    'COLLECTIVE_GROUP_BILL': BillType.COLLECTIVE_GROUP_BILL,
-    'CLIENT_BILL': BillType.CLIENT_BILL,
-    'INDIVIDUAL_BILL': BillType.INDIVIDUAL_BILL
-  };
+  displayedColumns = [
+    'deliveryDate', 'amount', 'paymentDate'];
+  datasource: MatTableDataSource<Bill>;
 
-  selectedBillType: string;
+  @ViewChild(MatSort) sort: MatSort;
+
+  _showFull = false;
+
+  _showUnpaid = false;
 
   constructor(private professionalService: ProfessionalService,
     private router: Router,
@@ -32,48 +34,21 @@ export class BillsComponent implements OnInit {
       (data: {
         bills: Bill[]
       }) => {
-        this._bills = data.bills;
+        this.bills = data.bills;
       }
     );
   }
 
-  initBills() {
-
-    // reset the service/pack sets
-    this.clientBills = [];
-    this.collectiveGroupBills = [];
-    this.individualBills = [];
-
-    // build the bills sets
-    this._bills.forEach(
-      b => {
-        switch (b.cltype) {
-          case BillType.CLIENT_BILL:
-            this.clientBills.push(b as ClientBill);
-            break;
-          case BillType.COLLECTIVE_GROUP_BILL:
-            this.collectiveGroupBills.push(b as CollectiveGroupBill);
-            break;
-          case BillType.INDIVIDUAL_BILL:
-            this.individualBills.push(b as IndividualBill);
-            break;
-          default:
-            throw new TypeError(`initBills(): ${b.cltype} is not a supported bill type`);
-        }
-      });
-    this._bills = []; // reset _bills, not used after initBills()
-  }
-
   ngOnInit() {
-
-    this.initBills();
-    // check the optional parameter bt (bill type)
-    this.route.paramMap.subscribe(params => {
-      this.selectedBillType = params.get('bt');
-      if (!this.selectedBillType) {
-        this.selectedBillType = BillType.CLIENT_BILL;
+    this.billsModel = this.bills.sort(// inverse order : most recent first
+      (b1, b2) => {
+        const diff = -moment(b1.deliveryDate).diff(moment(b2.deliveryDate));
+        return (!diff) ? -moment(b1.issueDate).diff(moment(b2.issueDate)) : diff;
       }
-    });
+    );
+    this.datasource =
+      new MatTableDataSource<Bill>(this.billsModel);
+    this.datasource.sort = this.sort;
   }
 
   refreshBills() {
@@ -82,12 +57,47 @@ export class BillsComponent implements OnInit {
       .finally(() => this.loadingOverlayService.stop())
       .subscribe(
       bills => {
-        this._bills = bills;
-        this.initBills();
+        this.bills = bills;
       },
       // TODO : handle the error
       e => console.error(`Une erreur est survenue lors de la collecte des factures sur le serveur`)
       );
+  }
+
+  get unpaid(): number {
+    return this.bills.filter(b => !b.paymentDate).length;
+  }
+
+  get showFull(): boolean {
+    return this._showFull && !this.billsIsEmpty();
+  }
+
+  get showUnpaid(): boolean {
+    return this._showUnpaid && !this.billsIsEmpty();
+  }
+
+  set showFull(value: boolean) {
+    this._showFull = value;
+    if (this._showFull) {
+      this._showUnpaid = !this._showFull;
+      this.datasource.data = this.billsModel;
+    }
+  }
+
+  set showUnpaid(value: boolean) {
+    this._showUnpaid = value;
+    if (this._showUnpaid) {
+      this._showFull = !this._showUnpaid;
+      this.datasource.data = this.billsModel.filter(b => !b.paymentDate);
+    }
+  }
+
+  billsIsEmpty(): boolean {
+    return this.bills.length === 0;
+  }
+
+  gotoClientBill(id: number, ref: string) {
+    this.router.navigate(['/professional/bills/clientbill', { id: id, ref: ref }]);
   }
 
 }
