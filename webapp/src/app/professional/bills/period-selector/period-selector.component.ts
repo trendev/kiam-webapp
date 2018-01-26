afd77ea980b1207bc6c3a46d277185b95df8b4f3
jsie <julien.sie@gmail.com>
2018-01-26 19:19:19 +0100

    add moment in period selector

--- webapp/src/app/professional/bills/period-selector/period-selector.component.ts
+++ webapp/src/app/professional/bills/period-selector/period-selector.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { MAT_DATE_LOCALE, DateAdapter, MAT_DATE_FORMATS } from '@angular/material';
+import { MomentDateAdapter, MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-period-selector',
   templateUrl: './period-selector.component.html',
-  styleUrls: ['./period-selector.component.scss']
+  styleUrls: ['./period-selector.component.scss'],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'fr-FR' },
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+  ]
 })
 export class PeriodSelectorComponent implements OnInit {
-
+  minDate: moment.Moment;
+  maxDate = moment({ hour: 0 });
   constructor() { }
 
   ngOnInit() {
