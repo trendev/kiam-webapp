cd866ce33ef3300e4eb751ca7b75061c95fbeaa8
jsie <julien.sie@gmail.com>
2018-02-01 20:45:30 +0100

    display error in snackBar

--- webapp/src/app/error-handler.service.ts
+++ webapp/src/app/error-handler.service.ts
@@ -4,13 +4,14 @@ import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
 import 'rxjs/add/observable/throw';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class ErrorHandlerService {
 
   errmsg: string;
 
-  constructor() { }
+  constructor(public snackBar: MatSnackBar) { }
 
   /**
    * Updates errmsg, logs it on the console and returns an ErrorObservable with the message
@@ -24,12 +25,25 @@ export class ErrorHandlerService {
       if (err.error instanceof Error) {
         this.errmsg =
           `A client-side or network error occurred: ${err.error.message}`;
-          console.error('ErrorHandlerService#handle => ' + this.errmsg);
+        console.error('ErrorHandlerService#handle => ' + this.errmsg);
       } else {
         this.errmsg =
           `The backend returned an unsuccessful response code: ${err.status} - ${err.statusText || ''} ${err.message} `;
         this.errmsg += JSON.stringify(err.error) || '';
         console.warn('ErrorHandlerService#handle => ' + this.errmsg);
+
+        if (err.status === 404 || err.status === 0) {
+          this.snackBar.open(`Oups, une erreur est survenue sur le réseau ` +
+            `ou l'application n'est pas en ligne pour le moment (maintenance en cours...)`,
+            `ERREUR`,
+            { duration: 5000 });
+        }
+
+        if (err.status === 401 || err.status === 403) {
+          this.snackBar.open(`Vous êtes actuellement déconnecté des services: veuillez vous connecter/reconnecter s'il vous plaît...`,
+            `ERREUR`,
+            { duration: 5000 });
+        }
       }
     }
     // return Observable.throw(this.errmsg);
