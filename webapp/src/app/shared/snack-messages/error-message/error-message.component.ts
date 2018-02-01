b29e4166cf0f2102979188e18aaf7f4918ebcae2
jsie <julien.sie@gmail.com>
2018-02-01 19:31:48 +0100

    add html template in error-message

--- webapp/src/app/shared/snack-messages/error-message/error-message.component.ts
+++ webapp/src/app/shared/snack-messages/error-message/error-message.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-error-message',
   templateUrl: './error-message.component.html',
   styleUrls: ['./error-message.component.scss']
 })
-export class ErrorMessageComponent implements OnInit {
+export class ErrorMessageComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
