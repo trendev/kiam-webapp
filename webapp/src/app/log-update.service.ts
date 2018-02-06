7f3cabd52a65d796ab322363b5c3f3c0f5b1d8cc
jsie <julien.sie@gmail.com>
2018-02-06 16:05:36 +0100

    open snackBar on update available/activated

--- webapp/src/app/log-update.service.ts
+++ webapp/src/app/log-update.service.ts
@@ -9,10 +9,16 @@ export class LogUpdateService {
     updates.available.subscribe(event => {
       console.log('current version is', event.current);
       console.log('available version is', event.available);
+      this.snackBar.open(`current version is ${event.current} / available version is ${event.available}`,
+        `update available`,
+        { duration: 3000 });
     });
     updates.activated.subscribe(event => {
       console.log('old version was', event.previous);
       console.log('new version is', event.current);
+      this.snackBar.open(`old version was ${event.previous} / new version is ${event.current}`,
+        `webapp updated`,
+        { duration: 3000 });
     });
   }
 }
