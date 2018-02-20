884093f9dcf911367acbfd6fdafb6975c2ef4b26
jsie <julien.sie@gmail.com>
2018-02-20 17:06:38 +0100

    import pdkmake + init exportbillservice

--- webapp/src/app/core/export-bill.service.ts
+++ webapp/src/app/core/export-bill.service.ts
@@ -1,5 +1,8 @@
 import { ClientBill } from './../entities/client-bill.model';
 import { Injectable } from '@angular/core';
+import * as pdfMake from 'pdfmake/build/pdfmake.js';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts.js';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Injectable()
 export class ExportBillService {
@@ -7,7 +10,8 @@ export class ExportBillService {
   constructor() { }
 
   exportClientBill(clientBill: ClientBill) {
-
+    const docDefinition = { content: JSON.stringify(clientBill) };
+    pdfMake.createPdf(docDefinition).download(`${clientBill.reference}.pdf`);
   }
 
 }
