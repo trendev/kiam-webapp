7a21ff379e7ee38e612bd0bbd63a9090fcedefbd
jsie <julien.sie@gmail.com>
2018-03-01 14:04:36 +0100

    hide mods in progress in sidenav

--- webapp/src/app/professional/professional-dashboard/professional-sidenav/professional-sidenav.component.ts
+++ webapp/src/app/professional/professional-dashboard/professional-sidenav/professional-sidenav.component.ts
@@ -13,19 +13,19 @@ export class ProfessionalSidenavComponent implements OnInit {
   @Input() sidenav: MatSidenav;
 
   links: LinkItem[] = [
-    { name: 'Accueil', url: 'welcome', icon: 'dashboard'},
-    { name: 'Clients', url: 'clients', icon: 'perm_contact_calendar'},
-    { name: 'Groupes / Collectivités', url: 'groups', icon: 'group'},
-    { name: 'Facturier', url: 'bills', icon: 'receipt'},
-    { name: 'Dépenses', url: 'expenses', icon: 'trending_down'},
-    { name: 'Catégories', url: 'categories', icon: 'label_outline'},
-    { name: 'Offres', url: 'offerings', icon: 'local_offer'},
+    { name: 'Offres', url: 'offerings', icon: 'local_offer' },
+    // { name: 'Accueil', url: 'welcome', icon: 'dashboard'},
+    { name: 'Clients', url: 'clients', icon: 'perm_contact_calendar' },
+    // { name: 'Groupes / Collectivités', url: 'groups', icon: 'group'},
+    { name: 'Facturier', url: 'bills', icon: 'receipt' },
+    // { name: 'Dépenses', url: 'expenses', icon: 'trending_down'},
+    // { name: 'Catégories', url: 'categories', icon: 'label_outline'},
   ];
 
   constructor() { }
 
   ngOnInit() {
-    this.links.sort((l1, l2) => l1.name.localeCompare(l2.name));
+    // this.links.sort((l1, l2) => l1.name.localeCompare(l2.name));
   }
 
 }
