3d5bbb7259c81464581df1f1e2a7f9053bd5bbaf
jsie <julien.sie@gmail.com>
2018-03-03 13:32:58 +0100

    init CreateCollectiveGroupComponent

--- webapp/src/app/professional/collective-groups/collective-groups.module.ts
+++ webapp/src/app/professional/collective-groups/collective-groups.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from '@angular/core';
 import { CollectiveGroupsComponent } from './collective-groups/collective-groups.component';
 import { SharedModule, CustomMatPaginatorIntlFr } from '@app/shared';
 import { MatPaginatorIntl } from '@angular/material';
+import { CreateCollectiveGroupComponent } from './create-collective-group/create-collective-group.component';
 
 @NgModule({
   imports: [
     SharedModule,
     CollectiveGroupsRoutingModule
   ],
-  declarations: [CollectiveGroupsComponent],
+  declarations: [CollectiveGroupsComponent, CreateCollectiveGroupComponent],
   providers: [
     { provide: MatPaginatorIntl, useClass: CustomMatPaginatorIntlFr }
   ],
---
+++ webapp/src/app/professional/collective-groups/create-collective-group/create-collective-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-collective-group',
+  templateUrl: './create-collective-group.component.html',
+  styleUrls: ['./create-collective-group.component.scss']
+})
+export class CreateCollectiveGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
