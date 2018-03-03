6bbcd55a1f48522ade0ab69836e8f338321175c8
jsie <julien.sie@gmail.com>
2018-03-03 15:19:59 +0100

    fix issue in message

--- webapp/src/app/professional/collective-groups/create-collective-group/create-collective-group.component.ts
+++ webapp/src/app/professional/collective-groups/create-collective-group/create-collective-group.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-collective-group',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-collective-group.component.scss']
 })
 export class CreateCollectiveGroupComponent implements OnInit {
+  form: FormGroup;
 
   constructor() { }
 
