2df142391a837db71f8e4aa8d0b73a13aeeba91f
jsie <julien.sie@gmail.com>
2018-03-12 11:51:19 +0100

    initClientModel / checked

--- webapp/src/app/professional/categories/category-detail/category-client-list/category-client-list.component.ts
+++ webapp/src/app/professional/categories/category-detail/category-client-list/category-client-list.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
+import { Client } from '@app/entities';
+import { MatTableDataSource, MatSort, MatPaginator, MatCheckboxChange } from '@angular/material';
 
 @Component({
   selector: 'app-category-client-list',
   templateUrl: './category-client-list.component.html',
   styleUrls: ['./category-client-list.component.scss']
 })
-export class CategoryClientListComponent implements OnInit {
+export class CategoryClientListComponent implements OnInit, AfterViewInit {
+
+  @Input() categoryClients: Client[] = [];
+  @Input() clients: Client[] = [];
+  clientsModel: ClientModel[];
+
+  displayedColumns = [
+    'checked', 'firstname', 'lastname'];
+  datasource: MatTableDataSource<ClientModel> = new MatTableDataSource<ClientModel>();
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  clientSortFn: (cl1: ClientModel, cl2: ClientModel) => number
+    = (cl1, cl2) => {
+      if (cl1.checked === cl2.checked) {
+        return cl1.lastname === cl2.lastname ?
+          cl1.firstname.localeCompare(cl2.firstname)
+          : cl1.lastname.localeCompare(cl2.lastname);
+      } else {
+        return cl1.checked < cl2.checked ? 1 : -1;
+      }
+    }
 
   constructor() { }
 
   ngOnInit() {
+    this.initClientModel();
+    this.datasource.data = this.clientsModel.sort(this.clientSortFn);
   }
 
+  ngAfterViewInit() {
+    this.datasource.sort = this.sort;
+    this.datasource.paginator = this.paginator;
+  }
+
+  initClientModel() {
+    this.clientsModel = this.clients.map(
+      cl => {
+        return {
+          checked: this.categoryClients.findIndex(_cl => _cl.id === cl.id) !== -1,
+          id: cl.id,
+          firstname: cl.customerDetails.firstName || '',
+          lastname: cl.customerDetails.lastName || ''
+        };
+      }
+    );
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.datasource.filter = filterValue;
+  }
+
+  select(event: MatCheckboxChange, element: ClientModel) {
+    element.checked = event.checked;
+    this.onSelection(element);
+  }
+
+  private onSelection(element: ClientModel) {
+    if (element.checked) {
+      // this.addOffering(element.offering);
+    } else {
+      // this.removeOffering(element.offering);
+    }
+  }
+
+}
+
+interface ClientModel {
+  checked: boolean;
+  firstname: string;
+  lastname: string;
+  id: number;
 }
