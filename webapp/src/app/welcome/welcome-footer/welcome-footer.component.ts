d7c83b345572a8e08d1f2862589bbecb340f0ffd
jsie <julien.sie@gmail.com>
2018-08-23 19:07:23 +0200

    update welcome footer + messages

--- webapp/src/app/welcome/welcome-footer/welcome-footer.component.ts
+++ webapp/src/app/welcome/welcome-footer/welcome-footer.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome-footer.component.scss']
 })
 export class WelcomeFooterComponent implements OnInit {
-
+  twitterUrl = 'https://twitter.com/comptandye';
+  facebookUrl = 'https://www.facebook.com/comptandye';
   constructor() { }
 
   ngOnInit() {
