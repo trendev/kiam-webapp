5fed9c8aadb1f550e2e5c3a78025525fc30bbfeb
jsie <julien.sie@gmail.com>
2018-09-13 09:45:42 +0200

    update welcome message

--- webapp/src/app/welcome/banner/sentence/sentence.component.ts
+++ webapp/src/app/welcome/banner/sentence/sentence.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-sentence',
   templateUrl: './sentence.component.html',
   styleUrls: ['./sentence.component.scss']
 })
-export class SentenceComponent implements OnInit {
+export class SentenceComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
