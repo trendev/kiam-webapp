c7426f1a7c940524fce2d9ef3c1afe446c48dcd1
jsie <julien.sie@gmail.com>
2018-09-22 19:53:05 +0200

    displau subscription from canceled_at

--- webapp/src/app/professional/profile/subscription-details/subscription-status/subscription-status.component.ts
+++ webapp/src/app/professional/profile/subscription-details/subscription-status/subscription-status.component.ts
@@ -1,5 +1,6 @@
 import { StripeSubscription } from './../stripe-subscription.model';
 import { Component, OnInit, Input } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-subscription-status',
@@ -15,6 +16,19 @@ export class SubscriptionStatusComponent implements OnInit {
 
   get isRescinded(): boolean {
     return !!this.subscription.canceled_at;
+  }
+
+  get canceled_at(): number {
+    return this.subscription.canceled_at;
+  }
+
+  get current_period_end(): string {
+    moment.locale('fr');
+    return moment.duration(moment(this.subscription.current_period_end).diff(moment(this.canceled_at))).humanize(true);
+  }
+
+  get removal_date(): Date {
+    return moment(this.subscription.current_period_end).add(14, 'days').startOf('day').toDate();
   }
 
 }
