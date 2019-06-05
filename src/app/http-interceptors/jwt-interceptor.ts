8e2a67e938e05b581bc8d4e5001e0a4be083acf8
Julien SIE <julien.sie@gmail.com>
2019-06-05 16:59:43 +0200

    implement + test jwt-interceptor

--- src/app/http-interceptors/jwt-interceptor.ts
+++ src/app/http-interceptors/jwt-interceptor.ts
@@ -1,20 +1,51 @@
 import { Injectable } from '@angular/core';
 import {
-    HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+    HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse
 } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
 @Injectable()
 export class JWTInterceptor implements HttpInterceptor {
+
+    private readonly JWT_HEADER = 'JWT';
 
     constructor() { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-        // TODO : load the token from the localstorage
-        const jwt = 'my_fake_token';
+        const jwt = this.loadJWT();
 
-        const authReq = req.clone({ setHeaders: { Authorization: 'Bearer ' + jwt } });
-        return next.handle(authReq);
+        if (jwt) {
+            const authReq = req.clone({ setHeaders: { Authorization: 'Bearer ' + jwt } });
+            return this.handle(authReq, next);
+
+        } else {
+            return this.handle(req, next);
+        }
+    }
+
+    private loadJWT(): string {
+        return localStorage.getItem(this.JWT_HEADER);
+    }
+
+    private saveJWT(jwt: string) {
+        if (jwt) {
+            localStorage.setItem(this.JWT_HEADER, jwt);
+        }
+    }
+
+    private handle(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req)
+            .pipe(
+                tap(event => {
+                    console.log(`event = ${JSON.stringify(event, null, 2)}`);
+                    if (event instanceof HttpResponse) {
+                        const jwt = event.headers.get(this.JWT_HEADER);
+                        this.saveJWT(jwt);
+                    }
+                })
+            );
     }
 }
