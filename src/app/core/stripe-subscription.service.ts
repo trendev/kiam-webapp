981f43cdc83aec1c533644e26fdb78da46ba7883
Julien Sié <julien.sie@gmail.com>
2019-07-24 14:34:31 +0200

    change subscription endpoint

--- src/app/core/stripe-subscription.service.ts
+++ src/app/core/stripe-subscription.service.ts
@@ -10,7 +10,7 @@ import { catchError } from 'rxjs/operators';
 })
 export class StripeSubscriptionService {
 
-  readonly api = `${environment.api}/StripeSubscription`;
+  readonly api = `${environment.api}/stipe-subscription`;
 
   constructor(private http: HttpClient) { }
 
