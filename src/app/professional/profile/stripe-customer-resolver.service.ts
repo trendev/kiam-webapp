fc08249f8314fcb3bc212b5ed3bb058d3f5cc8de
Julien Sié <julien.sie@gmail.com>
2019-08-02 20:21:53 +0200

    remove debug log

--- src/app/professional/profile/stripe-customer-resolver.service.ts
+++ src/app/professional/profile/stripe-customer-resolver.service.ts
@@ -2,7 +2,7 @@ import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/r
 import { StripeSubscriptionService } from '@app/core';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { ErrorHandlerService } from '@app/error-handler.service';
 
 @Injectable()
@@ -14,7 +14,6 @@ export class StripeCustomerResolverService implements Resolve<any> {
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any | Observable<any> | Promise<any> {
 
     return this.stripeSubscriptionService.details().pipe(
-      tap(c => console.log(c)),
       catchError(e => {
         this.errorHandler.handle(e, `Impossible de récupérer le détail de la souscription...`);
         return of(null);
