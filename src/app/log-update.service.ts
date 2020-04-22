2546cc5fc2b4cea4b216f41b272b34beead1e0aa
Julien Sié <julien.sie@gmail.com>
2020-04-22 14:57:59 +0200

    service worker update

--- src/app/log-update.service.ts
+++ src/app/log-update.service.ts
@@ -11,18 +11,20 @@ export class LogUpdateService {
       console.log('current version is', event.current.appData['version']);
       console.log('available version is', event.available.appData['version']);
 
-      this.snackBar.open(`version actuelle: ${event.current.hash} `
-        + `/ version disponible: ${event.available.hash}`,
-        `Mise à jour disponible`,
+      const snackBarRef = this.snackBar.open(`Mise à jour disponible`,
+        `Recharger`,
         { duration: 5000 });
+
+      snackBarRef.onAction().subscribe(() => {
+        window.location.reload(); // reload the current page with the new version
+      });
     });
+
     updates.activated.subscribe(event => {
       console.log('old version was', event.previous.appData['version']);
       console.log('new version is', event.current.appData['version']);
 
-      this.snackBar.open(`ancienne version: ${event.previous.hash} / nouvelle version: ${event.current.hash}`,
-        `Mise à jour effectuée`,
-        { duration: 5000 });
+      this.snackBar.open(`Mise à jour effectuée ✅`, `OK`);
     });
   }
 }
