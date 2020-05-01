71b6f1eeff2caa39198a1a050841bbb1e81840bb
Julien Sié <julien.sie@gmail.com>
2020-05-01 17:10:43 +0200

    filter cancel bill button

--- src/app/professional/bills/bill-detail/bill-detail.component.ts
+++ src/app/professional/bills/bill-detail/bill-detail.component.ts
@@ -60,8 +60,12 @@ export class BillDetailComponent implements OnInit, DoCheck {
     return this.form.get('payments').get('content');
   }
 
-  isClosedBill() {
+  get isClosedBill() {
     return this.bill.paymentDate ? true : false;
+  }
+
+  get isCancelled() {
+    return this.bill.cancelled && !!this.bill.cancellationDate;
   }
 
   createForm(): FormGroup {
@@ -72,13 +76,13 @@ export class BillDetailComponent implements OnInit, DoCheck {
         dates: this.fb.group({
           deliveryDate: new FormControl({ value: moment(this.bill.deliveryDate), disabled: true }),
           paymentDate: new FormControl({
-            value: this.isClosedBill() ? moment(this.bill.paymentDate) : undefined,
+            value: this.isClosedBill ? moment(this.bill.paymentDate) : undefined,
             disabled: true
           })
         }, { validator: CustomValidators.validDeliveryPaymentDates }),
         closeable: new FormControl({
-          value: this.isClosedBill(),
-          disabled: this.isClosedBill()
+          value: this.isClosedBill,
+          disabled: this.isClosedBill
         }),
         comments: this.fb.array(this.bill.comments || [],
           CustomValidators.validComments(this.commentsValidators))
