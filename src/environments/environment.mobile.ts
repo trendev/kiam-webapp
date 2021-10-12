fb00f6811f22e547fee84cd3fbef11fd77d0a7c1
Julien Sié <julien.sie@gmail.com>
2021-10-12 17:54:03 +0200

    update stripe pk

--- src/environments/environment.mobile.ts
+++ src/environments/environment.mobile.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   api: 'https://jsie-macbook:8181/api',
   title: 'kiam',
-  stripe_pk: 'pk_test_zw5JuBUOmXxIsi55rTtDDQG9'
+  stripe_pk: 'pk_test_51CuI1JI8ga14lIqaGnVV9HA5uXhpwf7xafP4gl1iXVWEdFQmR9bSQngnYLPvdtmr7iO3jDILsGMm7zFJ4TCk6n2o00IOMhAERf'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   api: '/api',
   title: 'kiam',
-  stripe_pk: 'pk_live_r69TK4H6ByEpqqbxYRf3rMZ1'
+  stripe_pk: 'pk_live_51CuI1JI8ga14lIqaMG1rDkALpbX6xSojae0mDc6196D1IXE5HQPyecvUaMF2NxLbZnFeN0TA4ZL37rcD9EFB9p9n00tJMDL9go'
 };
